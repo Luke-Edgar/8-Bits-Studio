366fc1588779a6441b58594cf65c1df0155f431e
Fab <s3562731@users.noreply.github.com>
2016-11-15 22:45:32 +1100

    Added Game UI, Gameover screen, pause menu

--- Alien Incursion/Assets/Scripts/AvatarController.cs
+++ Alien Incursion/Assets/Scripts/AvatarController.cs
@@ -1,73 +1,1 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class AvatarController : MonoBehaviour {
-
-	public bool facingRight;
-	public bool jump = false;
-
-	public float maxSpeed = 10f;
-	public float moveForce = 365f;
-	public float jumpForce = 150f;
-
-	private bool grounded = false;
-    private bool canDoubleJump;
-
-	private Rigidbody2D rb2d;
-    public Transform groundCheck;
-
-
-	// Use this for initialization
-	void Awake () 
-	{
-		rb2d = GetComponent<Rigidbody2D>();
-	}
-	
-	// Update is called once per frame
-	void Update ()
-	{
-		grounded = Physics2D.Linecast (transform.position, groundCheck.position, 1 << LayerMask.NameToLayer ("Ground"));
-
-		if (Input.GetButtonDown ("Jump")) {
-            if (grounded)
-            {
-                rb2d.AddForce(Vector2.up * jumpForce); 
-                canDoubleJump = true;
-            } else
-            {
-                if(canDoubleJump)
-                {
-                    canDoubleJump = false;
-                    rb2d.velocity = new Vector2(rb2d.velocity.x, 0);
-                    rb2d.AddForce(Vector2.up * jumpForce);
-                }
-            }
-        }
-	}
-
-	void FixedUpdate ()
-	{
-		float h = Input.GetAxis ("Horizontal");
-
-		if (h * rb2d.velocity.x < maxSpeed)
-			rb2d.AddForce (Vector2.right * h * moveForce);
-
-		if (Mathf.Abs (rb2d.velocity.x) > maxSpeed)
-			rb2d.velocity = new Vector2 (Mathf.Sign (rb2d.velocity.x) * maxSpeed, rb2d.velocity.y);
-
-		if (h > 0 && !facingRight)
-			Flip ();
-		else if (h < 0 && facingRight)
-			Flip ();
-
-	}
-
-
-	void Flip ()
-	{
-		facingRight = !facingRight;
-		Vector3 theScale = transform.localScale;
-		theScale.x *= -1;
-		transform.localScale = theScale;
-	}
-}
+﻿using UnityEngine;using System.Collections;public class AvatarController : MonoBehaviour {	public bool facingRight;	public bool jump = false;	public float maxSpeed = 10f;	public float moveForce = 365f;	public float jumpForce = 150f;	private bool grounded = false;    private bool canDoubleJump;	private Rigidbody2D rb2d;    public Transform groundCheck;    public Vector3 LastStartPos;    // Use this for initialization    void Awake () 	{		rb2d = GetComponent<Rigidbody2D>();	}		// Update is called once per frame	void Update ()	{		grounded = Physics2D.Linecast (transform.position, groundCheck.position, 1 << LayerMask.NameToLayer ("Ground"));		if (Input.GetButtonDown ("Jump")) {            if (grounded)            {                rb2d.AddForce(Vector2.up * jumpForce);                 canDoubleJump = true;            } else            {                if(canDoubleJump)                {                    canDoubleJump = false;                    rb2d.velocity = new Vector2(rb2d.velocity.x, 0);                    rb2d.AddForce(Vector2.up * jumpForce);                }            }        }	}	void FixedUpdate ()	{		float h = Input.GetAxis ("Horizontal");		if (h * rb2d.velocity.x < maxSpeed)			rb2d.AddForce (Vector2.right * h * moveForce);		if (Mathf.Abs (rb2d.velocity.x) > maxSpeed)			rb2d.velocity = new Vector2 (Mathf.Sign (rb2d.velocity.x) * maxSpeed, rb2d.velocity.y);		if (h > 0 && !facingRight)			Flip ();		else if (h < 0 && facingRight)			Flip ();	}	void Flip ()	{		facingRight = !facingRight;		Vector3 theScale = transform.localScale;		theScale.x *= -1;		transform.localScale = theScale;	}    void Start()    {        LastStartPos = transform.position;    }}
