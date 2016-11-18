5851717f4cc5be582459e49ca40c8bd2e9e1e9cb
Luke-Edgar <Luke-Edgar@users.noreply.github.com>
2016-11-18 23:42:56 +1000

    Finished the tutorial dialogue

--- Alien Incursion/Assets/Scripts/DialoguePause.cs
+++ Alien Incursion/Assets/Scripts/DialoguePause.cs
@@ -6,7 +6,10 @@ public class DialoguePause : MonoBehaviour {
 	public AvatarController avatar;
 	public int dialogueChoice = 0;
 
-	public string [] dialogues = { "Intro", "Melee", "Shield", "Boss" };
+	public string [] dialogues = { "The invading aliens mothership is directly above us inflitrate the mothership and capture or destroy it", 
+									"These basic melee enemies are the runts of the enemy species they will charge you and try to harm you at close range", 
+									"These shielded enemies are much like the basic melee enemies except can only be shot from behind", 
+									"The much larger \"Boss\" enemies are much stronger and fire high damage but slow moving plasma projectiles" };
 	string dialogue;
 
 	// Use this for initialization
